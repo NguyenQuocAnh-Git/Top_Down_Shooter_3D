59b89dff4a0e2117986d18116e3777f182f39c55
Nguyen Quoc Anh <anh.nguyen@topebox.com>
2026-06-12 03:01:30 +0700

    Enemy Misstion and Level

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    private int bulletDamage;
+    private float impactForce;
+
     private BoxCollider cd;
     private Rigidbody rb;
     private MeshRenderer meshRenderer;
@@ -15,7 +18,10 @@ public class Bullet : MonoBehaviour
     private float flyDistance;
     private bool bulletDisabled;
 
-    private void Awake()
+    private LayerMask allyLayerMask;
+
+
+    protected virtual void Awake()
     {
         cd = GetComponent<BoxCollider>();
         rb = GetComponent<Rigidbody>();
@@ -23,30 +29,35 @@ public class Bullet : MonoBehaviour
         trailRenderer = GetComponent<TrailRenderer>();
     }
 
-    public void BulletSetup(float flyDistance)
+    public void BulletSetup(LayerMask allyLayerMask, int bulletDamage, float flyDistance = 100, float impactForce = 100)
     {
+        this.allyLayerMask = allyLayerMask;
+        this.impactForce = impactForce;
+        this.bulletDamage = bulletDamage;
+
         bulletDisabled = false;
         cd.enabled = true;
         meshRenderer.enabled = true;
 
+        trailRenderer.Clear();
         trailRenderer.time = .25f;
         startPosition = transform.position;
         this.flyDistance = flyDistance + .5f; // magic number .5f is a length of tip of the laser ( Check method UpdateAimVisuals() on PlayerAim script) ;
     }
 
-    private void Update()
+    protected virtual void Update()
     {
         FadeTrailIfNeeded();
         DisableBulletIfNeeded();
         ReturnToPoolIfNeeded();
     }
 
-    private void ReturnToPoolIfNeeded()
+    protected void ReturnToPoolIfNeeded()
     {
         if (trailRenderer.time < 0)
             ReturnBulletToPool();
     }
-    private void DisableBulletIfNeeded()
+    protected void DisableBulletIfNeeded()
     {
         if (Vector3.Distance(startPosition, transform.position) > flyDistance && !bulletDisabled)
         {
@@ -55,31 +66,54 @@ public class Bullet : MonoBehaviour
             bulletDisabled = true;
         }
     }
-    private void FadeTrailIfNeeded()
+    protected void FadeTrailIfNeeded()
     {
         if (Vector3.Distance(startPosition, transform.position) > flyDistance - 1.5f)
             trailRenderer.time -= 2 * Time.deltaTime; // magic number 2 is choosen trhou testing
     }
 
-    private void OnCollisionEnter(Collision collision)
+
+
+    protected virtual void OnCollisionEnter(Collision collision)
     {
-        CreateImpactFx(collision);
+        if (FriendlyFare() == false)
+        {
+            // Use a bitwise AND to check if the collsion layer is in the allyLayerMask
+            if ((allyLayerMask.value & (1 << collision.gameObject.layer)) > 0)
+            {
+                ReturnBulletToPool(10);
+                return;
+            }
+        }
+
+        CreateImpactFx();
         ReturnBulletToPool();
+
+        IDamagable damagable = collision.gameObject.GetComponent<IDamagable>();
+        damagable?.TakeDamage(bulletDamage);
+
+        ApplyBulletImpactToEnemy(collision);
     }
 
-    private void ReturnBulletToPool() => ObjectPool.instance.ReturnObject(gameObject);
-
-
-    private void CreateImpactFx(Collision collision)
+    private void ApplyBulletImpactToEnemy(Collision collision)
     {
-        if (collision.contacts.Length > 0)
+        Enemy enemy = collision.gameObject.GetComponentInParent<Enemy>();
+        if (enemy != null)
         {
-            ContactPoint contact = collision.contacts[0];
-
-            GameObject newImpactFx = ObjectPool.instance.GetObject(bulletImpactFX);
-            newImpactFx.transform.position = contact.point;
-
-            ObjectPool.instance.ReturnObject(newImpactFx, 1);
+            Vector3 force = rb.velocity.normalized * impactForce;
+            Rigidbody hitRigidbody = collision.collider.attachedRigidbody;
+            enemy.BulletImpact(force, collision.contacts[0].point, hitRigidbody);
         }
     }
+
+    protected void ReturnBulletToPool(float delay = 0) => ObjectPool.instance.ReturnObject(gameObject, delay);
+
+
+    protected void CreateImpactFx()
+    {
+        GameObject newImpactFx = ObjectPool.instance.GetObject(bulletImpactFX, transform);
+        ObjectPool.instance.ReturnObject(newImpactFx, 1);
+    }
+
+    private bool FriendlyFare() => GameManager.instance.friendlyFire;
 }
